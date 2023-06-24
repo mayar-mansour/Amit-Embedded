065c54df2244f64487464dda1a5cc365898d5f52
mayar-mansour <mayarsoliman31@gmail.com>
2023-06-24 14:03:24 +0300

    PrintSemicolon

---
+++ C Taks/PrintSemicolon.c
@@ -0,0 +1,13 @@
+// 13.Write a program to print a semicolon without using a semicolon anywhere in the code.
+
+#include <stdio.h>
+
+int main()
+{
+    if (printf("%c", 59))
+    {
+        // Empty body
+    }
+
+    return 0;
+}
