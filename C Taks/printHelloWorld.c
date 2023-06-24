0bd10f0e3c3f39b8843aba690920b72975ad8410
mayar-mansour <mayarsoliman31@gmail.com>
2023-06-24 14:01:32 +0300

    printHelloWorld

---
+++ C Taks/printHelloWorld.c
@@ -0,0 +1,12 @@
+// 12.Write a program to print "Hello World" without using semicolon anywhere in the code.
+#include <stdio.h>
+
+int main()
+{
+    if (printf("Hello World\n"))
+    {
+        // Empty body
+    }
+
+    return 0;
+}
